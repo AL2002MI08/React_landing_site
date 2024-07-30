046843f0a914e171ad904d582b404aaa0e1902e6
AL2002MI08 <alexandramizero@gmail.com>
2024-07-30 20:12:36 +0200

    Add button to hero component

--- React-landing-page/src/components/Hero.jsx
+++ React-landing-page/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 export default function Hero(props) {
-    console.log(props)
+  console.log(props);
   return (
     <div className="">
       <div className="relative w-full h-screen">
@@ -8,9 +8,14 @@ export default function Hero(props) {
           alt="Background"
           className="absolute w-full h-full object-cover brightness-[75%]"
         />
-        <h2 className="text-center text-7xl font-bold text-white relative top-48 w-2/5 left-1/3 py-5">WE ARE A LANDING PAGE</h2>
-        <p className="text-center relative top-48 w-2/5 left-1/3 space-y-2 text-white font-light text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sed commodo nibh ante facilisis bibendum.</p>
-        <button className="border border-black">{props.button}</button>
+        <h2 className="text-center text-7xl font-bold text-white relative top-48 w-2/5 left-1/3 py-5">
+          WE ARE A LANDING PAGE
+        </h2>
+        <p className="text-center relative top-48 w-2/5 left-1/3 space-y-2 text-white font-light text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed
+          dapibus leo nec ornare diam sed commodo nibh ante facilisis bibendum.
+        </p>
+        <button className="border rounded-full relative top-48 left-1/2 uppercase px-10 py-3 my-6 bg-custom-blue text-white">Learn More</button>
       </div>
     </div>
   );
